000c56708b2280897b31cb5add89ec71797a7a10
Sahil Borada <boradasahil93@gmail.com>
2023-08-07 18:12:37 +0530

    common

---
+++ lib/common_widget/margin_common.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Widget horizontalSizeBox(double width) {
+  return SizedBox(width: width);
+}
+
+Widget verticalSizeBox(double height) {
+  return SizedBox(height: height);
+}
