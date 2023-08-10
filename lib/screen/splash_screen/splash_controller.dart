3366e247ecfccd78150e0cee694cd0c1e6fff02f
Sahil Borada <boradasahil93@gmail.com>
2023-08-10 18:13:09 +0530

    day puro

--- lib/screen/splash_screen/splash_controller.dart
+++ lib/screen/splash_screen/splash_controller.dart
@@ -6,7 +6,7 @@ import 'package:mudra_yojana/screen/home_screen/home_screen.dart';
 class SplashScreenController extends GetxController {
   @override
   void onInit() {
-    Timer(const Duration(seconds: 1), () => Get.off(() => const HomeScreen()));
+    Timer(const Duration(seconds: 5), () => Get.off(() => const HomeScreen()));
     super.onInit();
   }
 }
