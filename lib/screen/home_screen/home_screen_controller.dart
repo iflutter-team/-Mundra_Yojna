58945bfa2a3d54c76f55ccff179b303a8d62a634
Sahil Borada <boradasahil93@gmail.com>
2023-08-16 15:06:07 +0530

    ram rajya praja sukhi

--- lib/screen/home_screen/home_screen_controller.dart
+++ lib/screen/home_screen/home_screen_controller.dart
@@ -1,4 +1,6 @@
 import 'package:facebook_audience_network/facebook_audience_network.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mudra_yojana/api_model.dart';
 import 'package:mudra_yojana/screen/aadhar_with_pan_screen/addhar_with_pan.dart';
@@ -8,6 +10,8 @@ import 'package:mudra_yojana/screen/mudra_pmmy_screen/mudra_pmmy_screen.dart';
 
 class HomeController extends GetxController {
   ProductId? productId;
+  bool isRewardedAdLoaded = false;
+  bool isInterstitialAdLoaded = false;
 
   @override
   void onInit() {
@@ -19,7 +23,6 @@ class HomeController extends GetxController {
 
   Future<void> apiGet() async {
     productId = await HomeScreenApi.getData();
-    update(["Link Aadhaar with Pan Card"]);
   }
 
   Future<void> goToYojnaPage() async {
@@ -35,18 +38,67 @@ class HomeController extends GetxController {
   }
 
   Future<void> goToAadhaarWithPan() async {
-    await showInter();
     update(["Link Aadhaar with Pan Card"]);
     Get.to(() => const AadhaarWithPan());
   }
 
   showInter() {
-     FacebookInterstitialAd.loadInterstitialAd(
-       placementId: productId!.facebookInterstitialId!,
-       listener: (result, value) {
-         if (result == InterstitialAdResult.LOADED)
-           FacebookInterstitialAd.showInterstitialAd(delay: 2000);
-       },
-     );
+    FacebookInterstitialAd.loadInterstitialAd(
+      placementId: productId!.facebookInterstitialId!,
+      listener: (result, value) {
+        if (result == InterstitialAdResult.LOADED)
+          FacebookInterstitialAd.showInterstitialAd(delay: 2000);
+      },
+    );
+  }
+
+  showBannerAd() {
+    FacebookBannerAd(
+      placementId: productId!.facebookBannerId!,
+      bannerSize: BannerSize.STANDARD,
+      listener: (result, value) {
+        if (kDebugMode) {
+          print("Banner Ad: $result -->  $value");
+        }
+      },
+    );
+  }
+
+  Widget nativeBannerAd() {
+    return FacebookNativeAd(
+      placementId: "IMG_16_9_APP_INSTALL#2312433698835503_2964953543583512",
+      adType: NativeAdType.NATIVE_BANNER_AD,
+      bannerAdSize: NativeBannerAdSize.HEIGHT_100,
+      width: double.infinity,
+      backgroundColor: Colors.blue,
+      titleColor: Colors.white,
+      descriptionColor: Colors.white,
+      buttonColor: Colors.deepPurple,
+      buttonTitleColor: Colors.white,
+      buttonBorderColor: Colors.white,
+      listener: (result, value) {
+        print("Native Banner Ad: $result --> $value");
+      },
+    );
+  }
+
+  Widget nativeAd() {
+    return FacebookNativeAd(
+      placementId: productId!.facebookNativeId!,
+      adType: NativeAdType.NATIVE_AD_VERTICAL,
+      width: double.infinity,
+      height: 300,
+      backgroundColor: Colors.blue,
+      titleColor: Colors.white,
+      descriptionColor: Colors.white,
+      buttonColor: Colors.deepPurple,
+      buttonTitleColor: Colors.white,
+      buttonBorderColor: Colors.white,
+      listener: (result, value) {
+        print("Native Ad: $result --> $value");
+      },
+      keepExpandedWhileLoading: true,
+      expandAnimationDuraion: 1000,
+    );
   }
 }
